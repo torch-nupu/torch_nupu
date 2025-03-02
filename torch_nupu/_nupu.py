3577451a4243ffa961351ebf3ea349e6d604d6ba
Allen Guo <guosonglin001@gmail.com>
2025-03-02 06:25:41 +0000

    wip

--- torch_nupu/_nupu.py
+++ torch_nupu/_nupu.py
@@ -41,10 +41,26 @@ def current_stream(device=None):
     return torch_nupu._C._get_current_stream(device or current_device())
 
 
+class NupuDeviceProperties:
+    name: str = "refsi"
+    platform_name: str = "ComputeAortaOpenCL"
+    type: str = "gpu"
+    driver_version: str = "4.0"
+    total_memory = 2048 * 1024 * 1024  # 2048MB
+    max_compute_units: int = 1
+    gpu_eu_count: int = 512
+    gpu_subslice_count: int = 64
+    max_work_group_size: int = 1024
+    max_num_sub_groups: int = 1024
+    sub_group_sizes: list[int] = [32]
+    has_fp16: int = 1
+    has_fp64: int = 1
+    has_atomic64: int = 1
+
+
 def _get_device_properties(device=None):
-    # TODO: support API
-    print(f"_get_device_properties device:{device}")
-    pass
+    # TODO: support parse from opencl api
+    return NupuDeviceProperties()
 
 
 def get_device_properties(device=None):
