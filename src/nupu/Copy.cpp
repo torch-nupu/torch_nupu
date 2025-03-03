294e8c60c99b7c9b5a8d63658193e99019132630
Allen Guo <guosonglin001@gmail.com>
2025-03-03 14:44:06 +0000

    wip

--- src/nupu/Copy.cpp
+++ src/nupu/Copy.cpp
@@ -5,8 +5,7 @@
 #include <c10/util/Logging.h>
 
 #include <gen_functions/PrivateUse1NativeFunctions.h>
-
-using namespace at;
+#include "c10/core/DeviceType.h"
 
 namespace at_nupu {
 
@@ -16,6 +15,30 @@ at::Tensor& NupuNativeFunctions::copy_(
     bool non_blocking) {
   LOG(INFO) << "NupuNativeFunctions::copy_";
   // TODO: support copy_
+
+  LOG(INFO) << "self.data_ptr: " << self.data_ptr();
+  LOG(INFO) << "src.src: " << src.data_ptr();
+  LOG(INFO) << "self.device: " << self.device().str();
+  LOG(INFO) << "src.device: " << src.device().str();
+
+  if (self.device().type() == at::kCPU &&
+      src.device().type() == at::kPrivateUse1) {
+    LOG(INFO) << "nupu -> cpu";
+  } else if (
+      self.device().type() == at::kPrivateUse1 &&
+      src.device().type() == at::kCPU) {
+    LOG(INFO) << "cpu -> nupu";
+  } else if (
+      self.device().type() == at::kCPU && src.device().type() == at::kCPU) {
+    LOG(INFO) << "cpu -> cpu";
+  } else if (
+      self.device().type() == at::kPrivateUse1 &&
+      src.device().type() == at::kPrivateUse1) {
+    LOG(INFO) << "nupu -> nupu";
+  } else {
+    LOG(INFO) << "unknown copy_";
+  }
+
   return self;
 }
 
